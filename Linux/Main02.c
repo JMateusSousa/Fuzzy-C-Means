049151d7390fc910c9a79521e38f4add69513ecb
unknown <mateus_sousa_peu@hotmail.com>
2021-07-07 11:51:39 -0300

    Adding arduino's test

--- Linux/Main02.c
+++ Linux/Main02.c
@@ -1,13 +1,13 @@
-#include "FuzzyCMeans.c"
+#include "FuzzyCMeans02.c"
 
 int main(int argc, char **argv) {
     printf("------------------------------------------------------------------------\n");
-    fuzzy_c_means("Dataset\\WineDataset.dat");  
+    fuzzy_c_means();
     printf("Number of data points: %d\n", num_data_points);
     printf("Number of clusters: %d\n", num_clusters);
     printf("Number of data-point dimensions: %d\n", num_dimensions);
     printf("Accuracy margin: %lf\n", epsilon);
-    print_membership_matrix("Membership.matrix");
+    print_membership_matrix();
     printf("------------------------------------------------------------------------\n");
     return 0;
 }
